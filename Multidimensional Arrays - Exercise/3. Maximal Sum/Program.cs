50c789aed7e23b9799e012cc78a3055c268bcff1
Николай Тодоров <narcoswiu324@gmail.com>
2024-01-20 13:22:33 +0200

    Update exercise

--- Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
+++ Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
@@ -1,10 +1,71 @@
-﻿namespace _3._Maximal_Sum
+﻿using System.Runtime.InteropServices;
+
+namespace _3._Maximal_Sum
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int[] input = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            int[][] matrix = new int[rows][];
+
+            int maxSum = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+            //Read matrix from console
+            for (int row = 0; row < rows; row++)
+            {
+                
+                    matrix[row] = ReadIntArray();
+                
+            }
+
             
+            for (int row = 0; row < rows - 2; row++)
+            {
+                for (int col = 0; col < cols - 2; col++)
+                {
+                    int currentSum = 0;
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            currentSum += matrix[row + i][col + j];
+                        }
+                    }
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = maxRow; row < maxRow + 3; row++)
+            {
+                for (int col = maxCol; col < maxCol + 3; col++)
+                {
+                    Console.Write($"{matrix[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int[] ReadIntArray()
+        {
+            return Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
         }
     }
 }
