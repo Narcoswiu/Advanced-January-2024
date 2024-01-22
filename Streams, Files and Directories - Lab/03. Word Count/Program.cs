f49b974a3a1bca421f95239263b8c94589402921
Николай Тодоров <narcoswiu324@gmail.com>
2024-01-22 19:14:23 +0200

    Update exercises

---
+++ Streams, Files and Directories - Lab/03. Word Count/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(wordsFilePath))
+            {
+                using (StreamReader readerText = new StreamReader(textFilePath))
+                {
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+                        string[] words = reader.ReadToEnd().Split(" ");
+                        int count = 0;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            while (!readerText.EndOfStream && continue)
+                            {
+                                string line = readerText.ReadLine();
+                                if (line.Contains(words[i]))
+                                {
+                                    count++;
+                                }
+                            }
+                            writer.WriteLine($"{words[i]} - {count}");
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
--- Streams, Files and Directories - Lab/Streams, Files and Directories - Lab/Program.cs
+++ Streams, Files and Directories - Lab/Streams, Files and Directories - Lab/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Streams__Files_and_Directories___Lab
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System.Runtime.InteropServices;
 
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int line = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        writer.WriteLine($"{line++}. {reader.ReadLine()}");
+                    }
+                }
+            }
         }
     }
 }
