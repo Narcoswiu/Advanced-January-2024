fd7ba7fdb02a5c6068b1ae231d080f71eeb1ff1a
Николай Тодоров <narcoswiu324@gmail.com>
2024-01-28 11:54:24 +0200

    Add new exercise

---
+++ Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person first = new Person();
+            Person second = new Person(12);
+            Person third = new Person("Ivan",12);
+        }
+    }
+}
