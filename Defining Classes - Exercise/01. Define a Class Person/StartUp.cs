f78810484b5ecb114d191b3df9dc3fa2fda6b181
Николай Тодоров <narcoswiu324@gmail.com>
2024-01-28 11:43:39 +0200

    Add new lecture Defining Classes Exercise

---
+++ Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person newPerson = new Person();
+            newPerson.Age = 20;
+            newPerson.Name = "Peter";
+            newPerson.Age = 18;
+            newPerson.Name = "George";
+            newPerson.Age = 43;
+            newPerson.Name = "Jose";
+
+        }
+    }
+}
